5bf8b6900fad443401d82000fd70c3ef186892cd
Serena Serafini <xxladantessaxx@gmail.com>
2026-03-25 17:40:22 +0100

    Add ClicksCounter and Progressive Sum with code changes

---
+++ src/lib/components/ClicksCounter.svelte
@@ -0,0 +1,15 @@
+<script>
+    console.log('Hello world!');
+    let count = $state(0);
+    let doubled = $derived(count * 2);
+</script>
+
+<h3>Clicks Counter</h3>
+
+<button onclick={() => count++}>
+    clicks: {count}
+</button>
+
+<span>
+    doubled: {doubled}
+</span>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,18 +2,14 @@
     console.log('Hello world!');
     let count = $state(0);
     let doubled = $derived(count * 2);
+    import ClicksCounter from '$lib/components/ClicksCounter.svelte';
+    import ProgressiveSum from '$lib/components/ProgressiveSum.svelte';
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<button onclick={() => count++}>
-    clicks: {count}
-</button>
-
-<span>
-    doubled: {doubled}
-</span>
+<ClicksCounter />
 
 <style>
   h1 {
