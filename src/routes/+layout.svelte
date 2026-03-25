229d177d3214ac97acd3b32db9d8ae9da17c5651
Serena Serafini <xxladantessaxx@gmail.com>
2026-03-25 17:12:15 +0100

    Add style local and global

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children()}
+
+<footer>
+	<p>Copyright© Polimi 2026</p>
+</footer>
+
+<style>
+	:global(p) {
+		color: rgb(156, 10, 139);
+	}
+</style>
