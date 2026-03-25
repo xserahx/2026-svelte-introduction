89ff249d850ef571a3b92aeaede7823a4f84d0b5
Serena Serafini <xxladantessaxx@gmail.com>
2026-03-25 17:30:22 +0100

    Example of count and doubled count (state and derived)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,19 @@
+<script>
+    console.log('Hello world!');
+    let count = $state(0);
+    let doubled = $derived(count * 2);
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<button onclick={() => count++}>
+    clicks: {count}
+</button>
+
+<span>
+    doubled: {doubled}
+</span>
 
 <style>
   h1 {
