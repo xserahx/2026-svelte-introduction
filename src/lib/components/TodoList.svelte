8334f1a6285f338329c75bbaaef0c8ace86c3ee1
Serena Serafini <xxladantessaxx@gmail.com>
2026-04-11 19:50:12 +0200

    cleanup example of loops with snippet

---
+++ src/lib/components/TodoList.svelte
@@ -0,0 +1,38 @@
+<script>
+    let todos = $state([
+        { title: 'Assignment 1', done: false },
+        { title: 'Assignment 2', done: false },
+        { title: 'Assignment 3', done: false }
+    ]);
+
+    let groceries = $state([
+        { title: 'Milk', done: false },
+        { title: 'Bread', done: false },
+        { title: 'Eggs', done: false }
+    ]);
+</script>
+
+{#snippet todoItem(todo)}
+    <li>
+        <label>
+            <input type="checkbox" bind:checked={todo.done}>
+            {todo.title}
+        </label>
+    </li>
+{/snippet}
+
+<h3>Todo List</h3>
+
+<ul>
+    {#each todos as todo}
+        {@render todoItem(todo)}
+    {/each}
+</ul>
+
+<h3>Grocery List</h3>
+
+<ul>
+    {#each groceries as grocery}
+        {@render todoItem(grocery)}
+    {/each}
+</ul>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,14 @@
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
 <ClicksCounter bind:initialCount = {count} />
-<p> count from parent is {count} </p>
+
+{#if count > 10 && count < 20}
+  <p> count from parent is {count} </p>
+{:else if count > 30}
+  <p> wow! That's a lot of clicks!</p>
+{:else}
+  <p> count is not in range </p>
+{/if} 
 
 <Canvas />
 <style>
